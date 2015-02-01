0318481aa432bf3e89ac78c7cd62c375fcdca2dd
杨超 <1659514328@qq.com>
2015-02-01 19:53:23 +0800

    Signed-off-by: 杨超 <1659514328@qq.com>

---
+++ src/main/java/IndexAction1.java
@@ -0,0 +1,12 @@
+import com.opensymphony.xwork2.ActionSupport;
+
+
+public class IndexAction1 extends ActionSupport{
+	public String execute(){
+		return "success";
+	}
+	
+	public String addUser(){
+		return "success"; 
+	}
+} 
